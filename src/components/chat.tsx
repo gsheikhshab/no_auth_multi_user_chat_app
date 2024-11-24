e3577b81ad9ef017b73f56c0f9ab5784dffd304e
Golnar Sheikhshab <sheikhshab_bs@yahoo.com>
2024-11-24 13:33:26 -0800

    c

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,5 +1,3 @@
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
 
 
 export default function chatComp() {
