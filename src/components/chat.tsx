24f06e0fe9446e8f049439625688fa59147a604b
Golnar Sheikhshab <sheikhshab_bs@yahoo.com>
2024-11-24 13:30:37 -0800

    committing the new directory

---
+++ src/components/chat.tsx
@@ -0,0 +1,15 @@
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
+
+
+export default function chatComp() {
+
+	return (
+		<div style={{ position:"relative", height: "500px" }}>
+		  <p> Golnar says we are in chatComp </p>
+		</div>
+	)
+
+}
+
+
